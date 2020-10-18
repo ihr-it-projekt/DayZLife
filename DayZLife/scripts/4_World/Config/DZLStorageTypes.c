f7ae4ec0f2711ac4c063845d08491255ee17a180
Ralf Geisthardtz <RalfGe@gmail.com>
2020-10-18 21:23:19 +0200

    remove save logic

--- DayZLife/scripts/4_World/Config/DZLStorageTypes.c
+++ DayZLife/scripts/4_World/Config/DZLStorageTypes.c
@@ -3,27 +3,9 @@ class DZLStorageTypes
     ref array<ref DZLStorageType> storageTypes;
 
     void DZLStorageTypes() {
-        if (!Load()) {
-            storageTypes = new array<ref DZLStorageType>;
+        storageTypes = new array<ref DZLStorageType>;
 
-            storageTypes.Insert(new DZLStorageType("SeaChest", 5, 100));
-            storageTypes.Insert(new DZLStorageType("SeaChest", 50, 200));
-            Save();
-        }
-    }
-
-    private bool Load(){
-        if (GetGame().IsServer() && FileExist(DAY_Z_LIFE_SERVER_FOLDER_CONFIG + "storageTypes.json")) {
-            JsonFileLoader<DZLStorageTypes>.JsonLoadFile(DAY_Z_LIFE_SERVER_FOLDER_CONFIG + "storageTypes.json", this);
-            return true;
-        }
-        return false;
-    }
-
-    private void Save(){
-        if (GetGame().IsServer()) {
-            CheckDZLConfigPath();
-            JsonFileLoader<DZLStorageTypes>.JsonSaveFile(DAY_Z_LIFE_SERVER_FOLDER_CONFIG + "storageTypes.json", this);
-        }
+        storageTypes.Insert(new DZLStorageType("SeaChest", 5, 100));
+        storageTypes.Insert(new DZLStorageType("SeaChest", 50, 200));
     }
 }
