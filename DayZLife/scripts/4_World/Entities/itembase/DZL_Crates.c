e129943fa090800bfe7c59ed79c748f8e6d96bf1
Ralf Geisthardtz <RalfGe@gmail.com>
2020-10-19 08:16:56 +0200

    remove unused code

--- DayZLife/scripts/4_World/Entities/itembase/DZL_Crates.c
+++ DayZLife/scripts/4_World/Entities/itembase/DZL_Crates.c
@@ -2,19 +2,11 @@ class DZL_Crate_Base : Container_Base
 {
 	override bool CanPutIntoHands(EntityAI parent)
 	{
-		if( !super.CanPutIntoHands(parent) )
-		{
-			return false;
-		}
 		return false;
 	}
 	
 	override bool CanPutInCargo(EntityAI parent)
 	{
-		if( !super.CanPutInCargo(parent) )
-		{
-			return false;
-		}
 		return false;
 	}
 };
