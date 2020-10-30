e0ae53d7b81a0da6171a0bfbe7cb5a727596d3bc
Ralf Geisthardtz <RalfGe@gmail.com>
2020-10-30 13:07:58 +0100

    try fixing issue

--- DayZLife/scripts/4_World/Action/Continuoues/ActionHarvestItem.c
+++ DayZLife/scripts/4_World/Action/Continuoues/ActionHarvestItem.c
@@ -56,32 +56,22 @@ class ActionHarvestItem: ActionContinuousBase
 		    config = player.config.jobConfig;
 		}
 
-		DebugMessageDZL("has config ????");
 		if (!config) return false;
-		DebugMessageDZL("has config");
-		
-		
+
 		vector playerPosition = player.GetPosition();
 		
 		DZLWorkZone zone = FindZone(playerPosition, config);
 		if (zone) {
-			DebugMessageDZL("has zone");
 			foreach(DZLHarvestItemToolRelation relation: zone.harvestItemToolRelation) {
-			    DebugMessageDZL("check relation");
                 foreach(string _item: relation.itemsThatNeededForHarvest) {
-                    DebugMessageDZL("check item "+ handItemType + " " + _item);
                     if (handItemType.ToLower() == _item.ToLower()) {
-						if (GetGame().IsServer() && 0 == item_in_hands_source.GetHealth()) {
-							DebugMessageDZL("not item health");
-                            return false;
+						if (GetGame().IsServer()){
+						    return 0 < item_in_hands_source.GetHealth();
                         } 
-						DebugMessageDZL("all good");
                         return true;
                     }
 				}
 			}
-		} else {
-			DebugMessageDZL("no zone");
 		}
 
 		return false;
@@ -91,58 +81,63 @@ class ActionHarvestItem: ActionContinuousBase
 		return "#start_work";
 	}
 	
-	override void OnUpdate(ActionData action_data) {
-		super.OnUpdate(action_data);
-		if (action_data.m_State == UA_FINISHED && done == false) {
-			done = true;
+	override void OnUpdateServer(ActionData action_data) {
+		super.OnUpdateServer(action_data);
+		if (GetGame().IsServer()) {
+            if (action_data.m_State == UA_FINISHED && done == false) {
+                done = true;
 
-			EntityAI item_in_hands_source = action_data.m_Player.GetHumanInventory().GetEntityInHands();
-            if(!item_in_hands_source) return;
+                EntityAI item_in_hands_source = action_data.m_Player.GetHumanInventory().GetEntityInHands();
+                if(!item_in_hands_source) return;
 
-            string handItemType = item_in_hands_source.GetType();
-			
-			DZLWorkZone zone = FindZone(action_data.m_Player.GetPosition(), GetConfig());
+                string handItemType = item_in_hands_source.GetType();
 
-			if (zone) {
-			    array<DZLHarvestItemToolRelation> matchedRelations = new array<DZLHarvestItemToolRelation>;
+                DZLWorkZone zone = FindZone(action_data.m_Player.GetPosition(), GetConfig());
 
-			    foreach(DZLHarvestItemToolRelation relation: zone.harvestItemToolRelation) {
-                    foreach(string _item: relation.itemsThatNeededForHarvest) {
-                        if (handItemType == _item) {
-                            if (0 == item_in_hands_source.GetHealth()) {
-                                matchedRelations.Insert(relation);
+                if (zone) {
+                    array<DZLHarvestItemToolRelation> matchedRelations = new array<DZLHarvestItemToolRelation>;
+
+                    foreach(DZLHarvestItemToolRelation relation: zone.harvestItemToolRelation) {
+                        foreach(string _item: relation.itemsThatNeededForHarvest) {
+                            if (handItemType == _item) {
+                                if (0 == item_in_hands_source.GetHealth()) {
+                                    matchedRelations.Insert(relation);
+                                }
                             }
                         }
                     }
+
+                    if (matchedRelations.Count() == 0) return;
+
+                    DZLHarvestItemToolRelation randRelation = matchedRelations.GetRandomElement();
+
+                    if (!randRelation) return;
+
+                    string randomItemType = randRelation.itemsThatCanHarvest.GetRandomElement();
+
+                    InventoryLocation inventoryLocation = new InventoryLocation;
+
+                    EntityAI item;
+
+                    DebugMessageDZL("ITem:"+  randomItemType);
+
+                    if (action_data.m_Player.GetInventory().FindFirstFreeLocationForNewEntity(randomItemType, FindInventoryLocationType.ANY, inventoryLocation)) {
+                        item = action_data.m_Player.GetHumanInventory().CreateInInventory(randomItemType);
+                    } else if (!action_data.m_Player.GetHumanInventory().GetEntityInHands()) {
+                        item = action_data.m_Player.GetHumanInventory().CreateInHands(randomItemType);
+                    }
+
+                    if (!item) {
+                        GetGame().RPCSingleParam(action_data.m_Player, ERPCs.RPC_USER_ACTION_MESSAGE, new Param1<string>("#no_space_left_in_inventory"), true, action_data.m_Player.GetIdentity());
+                    }
+
+                    item_in_hands_source.SetHealth(item_in_hands_source.GetHealth() - zone.damagePerHarvestItem);
                 }
 
-				if (matchedRelations.Count() == 0) return;
-				
-				DZLHarvestItemToolRelation randRelation = matchedRelations.GetRandomElement();
-				
-				if (!randRelation) return;
-				
-				string randomItemType = randRelation.itemsThatCanHarvest.GetRandomElement();
-
-                InventoryLocation inventoryLocation = new InventoryLocation;
-
-                EntityAI item;
-				if (action_data.m_Player.GetInventory().FindFirstFreeLocationForNewEntity(randomItemType, FindInventoryLocationType.ANY, inventoryLocation)) {
-                    item = action_data.m_Player.GetHumanInventory().CreateInInventory(randomItemType);
-                } else if (!action_data.m_Player.GetHumanInventory().GetEntityInHands()) {
-                    item = action_data.m_Player.GetHumanInventory().CreateInHands(randomItemType);
-                }
-
-                if (!item) {
-                    GetGame().RPCSingleParam(action_data.m_Player, ERPCs.RPC_USER_ACTION_MESSAGE, new Param1<string>("#no_space_left_in_inventory"), true, action_data.m_Player.GetIdentity());
-                }
-
-				item_in_hands_source.SetHealth(item_in_hands_source.GetHealth() - zone.damagePerHarvestItem);
-			}
-		    
-		} else if ((action_data.m_State == UA_CANCEL || action_data.m_State == UA_INTERRUPT || action_data.m_State == UA_FAILED) && done == false) {
-		    done = true;
-		}
+            } else if ((action_data.m_State == UA_CANCEL || action_data.m_State == UA_INTERRUPT || action_data.m_State == UA_FAILED) && done == false) {
+                done = true;
+            }
+        }
 	}
 
 	override void OnStartServer(ActionData action_data) {
@@ -152,13 +147,11 @@ class ActionHarvestItem: ActionContinuousBase
 
 	private DZLWorkZone FindZone(vector playerPosition, DZLJobConfig config) {
 	    if (!playerPosition) {
-			DebugMessageDZL("No player pos");
             return null;
         }
 
 	    foreach(DZLWorkZone zone: config.workZones) {
             if(vector.Distance(zone.position, playerPosition) <= zone.range) {
-				DebugMessageDZL("is in range");
                 return zone;
             }
         }
