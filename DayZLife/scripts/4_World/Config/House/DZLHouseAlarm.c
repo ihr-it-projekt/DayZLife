d5171cdee866cc6730aa3c5cff6d98b0ca94af0e
Sense <markus.martin1985@web.de>
2020-12-02 18:53:35 +0100

    fix message

--- DayZLife/scripts/4_World/Config/House/DZLHouseAlarm.c
+++ DayZLife/scripts/4_World/Config/House/DZLHouseAlarm.c
@@ -9,18 +9,4 @@ class DZLHouseAlarm : DZLHouseExtension
         this.message = message;
         isHouseAlarm = true;
     }
-
-    override string GetMessage(PlayerBase raider, DZLHouse house) {
-		DebugMessageDZL("use DZLHouseAlarm");
-        if (level == 1) {
-            return message;
-        } else if (level == 2) {
-            return message + " " + house.name;
-        } else if (level == 3) {
-            return message + " " + house.name + "/" + raider.GetIdentity().GetName();
-        }
-
-        return "";
-    }
-
 }
