4bc20b78e00ad95796dc38fa6f2e63d009f26e68
Ralf Geisthardtz <RalfGe@gmail.com>
2020-12-03 21:39:38 +0100

    add almanac menu

--- DayZLife/scripts/4_World/Config/Job/Entity/DZLWorkZone.c
+++ DayZLife/scripts/4_World/Config/Job/Entity/DZLWorkZone.c
@@ -6,8 +6,10 @@ class DZLWorkZone
     int timeToHarvestPerItemInSeconds;
     int damagePerHarvestItem;
     ref array<ref DZLHarvestItemToolRelation> harvestItemToolRelation;
+	string zoneDisplayName;
 
     void DZLWorkZone(int m_CommandUID, vector position, float range, string zoneDisplayName, int timeToHarvestPerItemInSeconds, int damagePerHarvestItem, array<ref DZLHarvestItemToolRelation> harvestItemToolRelation) {
+		this.zoneDisplayName = zoneDisplayName;
         this.m_CommandUID = m_CommandUID;
         this.position = position;
         this.range = range;
