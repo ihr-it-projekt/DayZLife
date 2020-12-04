9eb3ebb8e13b75c6111763016e5ab37ed95ac754
Ralf Geisthardtz <RalfGe@gmail.com>
2020-12-04 18:58:51 +0100

    open menu with key combo

--- DayZLife/scripts/5_Mission/MissionGameplay.c
+++ DayZLife/scripts/5_Mission/MissionGameplay.c
@@ -1,24 +1,55 @@
- modded class MissionGameplay
- {
+modded class MissionGameplay{
+    private bool holdRControl = false;
+    private bool holdLControl = false;
 
+    PlayerBase player
 
- 	 override void OnKeyRelease(int key) {
- 	    PlayerBase player = PlayerBaseHelper.GetPlayer();
+    override void OnKeyRelease(int key) {
+        player = PlayerBaseHelper.GetPlayer();
 
- 	    if (!player) return;
+        if (!player) return;
 
-		super.OnKeyRelease(key);
-		 switch (key){
-			case KeyCode.KC_ESCAPE:
-				player.CloseMenu();
-				break;
-			case KeyCode.KC_RCONTROL:
-				GetGame().GetUIManager().ShowScriptedMenu(player.GetAlmanacMenu(), NULL);
-				break;
-			default:
-				break;
-		}
-		 
-	 }
-	
+        switch (key){
+            case KeyCode.KC_ESCAPE:
+                player.CloseMenu();
+                break;
+            case KeyCode.KC_RCONTROL:
+                holdRControl = false;
+                break;
+            case KeyCode.KC_LCONTROL:
+                holdLControl = false;
+                break;
+            default:
+                super.OnKeyRelease(key);
+                break;
+        }
+    }
+
+    override void OnKeyPress(int key) {
+        player = PlayerBaseHelper.GetPlayer();
+        if (!player) return;
+
+        player = PlayerBaseHelper.GetPlayer();
+
+        switch (key){
+            case KeyCode.KC_RCONTROL:
+                holdRControl = true;
+
+                if (holdRControl && holdLControl) {
+                    GetGame().GetUIManager().ShowScriptedMenu(player.GetAlmanacMenu(), NULL);
+                }
+
+                break;
+            case KeyCode.KC_LCONTROL:
+                holdLControl = true;
+                if (holdRControl && holdLControl) {
+                    GetGame().GetUIManager().ShowScriptedMenu(player.GetAlmanacMenu(), NULL);
+                }
+
+                break;
+            default:
+                super.OnKeyPress(key);
+                break;
+        }
+    }
  }
