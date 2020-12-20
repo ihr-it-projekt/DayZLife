df9d3f3ce2c68efc3e1701d0dc48916f9aba42e8
Sense <markus.martin1985@web.de>
2020-12-20 15:31:47 +0100

    add pos, kleidung

--- DayZLife/scripts/4_World/Config/Job/DZLArrestConfig.c
+++ DayZLife/scripts/4_World/Config/Job/DZLArrestConfig.c
@@ -8,21 +8,35 @@ class DZLArrestConfig
 	ref array<string> exPrisonerItems;
 	ref array<vector> arrestAreas;
 	ref array<vector> exPrisonerAreas;
-	int arrestAreaRadius = 10;
+	int arrestAreaRadius = 110;
 
     void DZLArrestConfig() {
         if (!Load()) {
             prisonerItems = new array<string>;
             prisonerItems.Insert("Pickaxe");
+            prisonerItems.Insert("PrisonerCap");
+            prisonerItems.Insert("PrisonUniformPants");
+            prisonerItems.Insert("PrisonUniformJacket");
+            prisonerItems.Insert("AthleticShoes_Black");
+            prisonerItems.Insert("SodaCan_Cola");
+            prisonerItems.Insert("SodaCan_Cola");
+            prisonerItems.Insert("TunaCan");
+            prisonerItems.Insert("TunaCan");
 
             exPrisonerItems = new array<string>;
-            exPrisonerItems.Insert("Crowbar");
+            exPrisonerItems.Insert("CanvasPants_Blue");
+            exPrisonerItems.Insert("TShirt_RedBlackStripes");
+            exPrisonerItems.Insert("AthleticShoes_Brown");
+            exPrisonerItems.Insert("SodaCan_Kvass");
+            exPrisonerItems.Insert("SodaCan_Kvass");
+            exPrisonerItems.Insert("TunaCan");
+            exPrisonerItems.Insert("TunaCan");
 
             arrestAreas = new array<vector>;
-			arrestAreas.Insert("4665.000000 339.282990 10305.000000");
+			arrestAreas.Insert("2746.173340 26.421398 1301.691162");
 
             exPrisonerAreas = new array<vector>;
-			exPrisonerAreas.Insert("4645.000000 339.282990 10305.000000");
+			exPrisonerAreas.Insert("3662.574219 6.720572 2396.926514");
 
             Save();
         }
