3c1a69b8cef726e04d225400ee925934902c6c8f
Ralf Geisthardtz <RalfGe@gmail.com>
2021-01-04 18:42:25 +0100

    add rand chance to car raid

--- DayZLifeServer/scripts/4_World/EventListener/DZLCarKeyListener.c
+++ DayZLifeServer/scripts/4_World/EventListener/DZLCarKeyListener.c
@@ -1,7 +1,10 @@
 class DZLCarKeyListener
 {
+	DZLCarConfig carConfig; 
+	
     void DZLCarKeyListener() {
         GetDayZGame().Event_OnRPC.Insert(HandleEventsDZL);
+		carConfig = DZLConfig.Get().carConfig;
     }
 
     void ~DZLCarKeyListener() {
@@ -46,12 +49,19 @@ class DZLCarKeyListener
         } else if (rpc_type == DAY_Z_LIFE_EVENT_CAR_RAID) {
             Param3<PlayerBase, CarScript, EntityAI> paramRaidCar;
             if (ctx.Read(paramRaidCar)){
-                CarScript raidedCar = paramRaidCar.param2;
-                raidedCar.isRaided = true;
-                raidedCar.SynchronizeValues(null);
-                paramRaidCar.param3.SetHealth(0);
+                int raidIndex = Math.RandomIntInclusive(1, carConfig.chanceToRaid);
+				
+				if (raidIndex == 1) {
+					CarScript raidedCar = paramRaidCar.param2;
+	               	raidedCar.isRaided = true;
+	               	raidedCar.SynchronizeValues(null);
+	               	paramRaidCar.param3.SetHealth(0);
+	
+	               	DZLSendMessage(sender, "#car_was_successful_raided");
+					return;
+				}
 
-                DZLSendMessage(sender, "#car_was_successful_raided");
+                DZLSendMessage(sender, "#car_raid_has_failed");
             }
         }
     }
