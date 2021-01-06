bff74c79421b887956658cf39f57d44baab0b524
Ralf Geisthardtz <RalfGe@gmail.com>
2021-01-06 19:40:35 +0100

    fix error

--- DayZLife/scripts/4_World/Entities/DayZPlayerImplement.c
+++ DayZLife/scripts/4_World/Entities/DayZPlayerImplement.c
@@ -1,7 +1,7 @@
 modded class DayZPlayerImplement
 {
     override bool HandleDeath(int pCurrentCommandID) {
-        bool isDead = super.HandleDeath(pCurrentCommandID);
+		bool isDead = super.HandleDeath(pCurrentCommandID);
 
         if (isDead && GetGame().IsServer()) {
             PlayerBase player = PlayerBase.Cast(this);
@@ -9,8 +9,8 @@ modded class DayZPlayerImplement
             if (player) {
                 DZLPlayer dzlPlayer = DZLDatabaseLayer.Get().GetPlayer(player.GetIdentity().GetId());
 
-                if (dzlPlayer.money > 0) {
-                    player.SetMoneyPlayerIsDead(dzlPlayer.money);
+                if (dzlPlayer.HasMoney()) {
+                    player.SetMoneyPlayerIsDead(dzlPlayer.GetMoney());
                     dzlPlayer.PlayerHasDied();
                     GetGame().RPCSingleParam(player, DAY_Z_LIFE_PLAYER_DATA_RESPONSE, new Param1<ref DZLPlayer>(dzlPlayer), true, player.GetIdentity());
                 }
