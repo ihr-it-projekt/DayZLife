b3326fd5d933f641435d2e23fbe9db9625a3a2e5
Ralf Geisthardtz <RalfGe@gmail.com>
2021-01-09 17:24:56 +0100

    add reset heal

--- DayZLife/scripts/4_World/Entities/ManBase/PlayerBase.c
+++ DayZLife/scripts/4_World/Entities/ManBase/PlayerBase.c
@@ -561,6 +561,7 @@ modded class PlayerBase
         SetPosition(point.point);
         SetOrientation(point.orientation);
         SyncMedicPlayer();
+        resetHealTimer.Run(60, this, "ResetHeal", null, false);
 	}
 
 	void HealByMedic() {
