6e4c785afcc57cbf1c70e83c9fae8b1e1840cfc7
Ralf Geisthardtz <RalfGe@gmail.com>
2021-01-11 19:27:37 +0100

    remove unneeded code

--- DayZLifeServer/scripts/4_World/TimeControlled/Player/DZLMedicHealth.c
+++ DayZLifeServer/scripts/4_World/TimeControlled/Player/DZLMedicHealth.c
@@ -3,18 +3,6 @@ class DZLMedicHealth
 	static void CheckHealth(PlayerBase player, PlayerIdentity playerIdentity) {
 	    bool showMedicHelpMenu = false;
 
-        if (player.GetHealth("GlobalHealth", "Health") <= 0) {
-            showMedicHelpMenu = true;
-            player.SetHealth("GlobalHealth", "Health", 0);
-            player.SetHealth("GlobalHealth", "Shock", 0);
-        }
-
-        if (player.GetHealth("GlobalHealth", "Blood") <= 0) {
-            showMedicHelpMenu = true;
-            player.SetHealth("GlobalHealth", "Blood", 0);
-            player.SetHealth("GlobalHealth", "Shock", 0);
-        }
-
         if (2 > player.GetHealth("GlobalHealth", "Shock")) {
             showMedicHelpMenu = true;
             player.SetHealth("GlobalHealth", "Shock", 0);
