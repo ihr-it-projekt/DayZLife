dbad067e892ad4185ed60e68f88235f261709838
Ralf Geisthardtz <RalfGe@gmail.com>
2021-01-16 21:03:01 +0100

    fix display arrest reason

--- DayZLife/scripts/4_World/Menu/DZLAlmanacMenu.c
+++ DayZLife/scripts/4_World/Menu/DZLAlmanacMenu.c
@@ -498,7 +498,7 @@ class DZLAlmanacMenu : DZLBaseMenu
 
 				foreach(DZLEscapedPlayer escapedPlayer: escapedPlayersParam) {
 					int indexOfRow = escapedPlayers.AddItem(escapedPlayer.name, escapedPlayer.player, 0);
-					escapedPlayers.SetItem(indexOfRow, escapedPlayer.arrestReason, escapedPlayer.player, 2);
+					escapedPlayers.SetItem(indexOfRow, escapedPlayer.arrestReason, escapedPlayer.player, 1);
 					if (player.GetDZLPlayer().IsActiveAsCop()) {
 						escapedPlayers.SetItem(indexOfRow, escapedPlayer.arrestTime.ToString(), escapedPlayer.player, 2);
 					} else {
