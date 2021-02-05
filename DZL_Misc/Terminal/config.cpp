66e281ff2c498ff91fba242b2b0d68601af0fbcc
Sense <markus.martin1985@web.de>
2021-02-05 17:05:27 +0100

    Terminal update

---
+++ DZL_Misc/Terminal/config.cpp
@@ -0,0 +1,23 @@
+class CfgPatches
+{
+	class DZL_Terminal
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Data"
+		};
+	};
+};
+
+class CfgVehicles
+{
+	class HouseNoDestruct;
+	class DZL_Terminal: HouseNoDestruct
+	{
+		scope=2;
+		model="\DZL_Misc\Terminal\DZL_Terminal.p3d";
+	};
+};
