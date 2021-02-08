bfa61f8919c44b2159175fc19e0e6490ff9d0dc3
Ralf Geisthardtz <RalfGe@gmail.com>
2021-02-08 17:37:35 +0100

    remove raid range from config

--- DayZLife/scripts/4_World/Config/House/DZLJobHouseDefinition.c
+++ DayZLife/scripts/4_World/Config/House/DZLJobHouseDefinition.c
@@ -1,13 +1,11 @@
 class DZLJobHouseDefinition {
 	
    	string houseType;
-	int raidRange;
 	ref array<string> raidTools;
 	int raidTimeInSeconds; 
 
-    void DZLJobHouseDefinition(string houseType, int raidRange = 10, int raidTimeInSeconds = 1200) {
+    void DZLJobHouseDefinition(string houseType, int raidTimeInSeconds = 1200) {
         this.houseType = houseType;
-        this.raidRange = raidRange;
         this.raidTimeInSeconds = raidTimeInSeconds;
 
         raidTools = new array<string>;
