e9563c899fcec69abe43dbbd4fbbb44f06f57cec
Ralf Geisthardtz <RalfGe@gmail.com>
2023-08-31 18:23:01 +0200

    fix respawn

--- DayZLife/scripts/3_Game/DayZGame.c
+++ DayZLife/scripts/3_Game/DayZGame.c
@@ -1,14 +1,9 @@
 modded class DayZGame
 {
 
-    override void EnterLoginTime(UIMenuPanel parent) {}
-	
-	
-	override void OnRespawnEvent(int time)	{	
-		if (GetPlayer())
-			GetPlayer().StopDeathDarkeningEffect();
-		PPERequesterBank.GetRequester(PPERequester_DeathDarkening).Start(new Param1<float>(1.0));
+    override void OnRespawnEvent(int time)	{
+        super.OnRespawnEvent(2);
 
-		PPEffects.SetUnconsciousnessVignette(0);
-	}
+        if (m_LoginTimeScreen)m_LoginTimeScreen.Hide();
+    }
 }
