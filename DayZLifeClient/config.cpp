e1b628a7d5aa78285de4e9e0f430d7448864b207
Ralf Geisthardtz <RalfGe@gmail.com>
2024-03-20 15:55:26 +0100

    format

--- DayZLifeClient/config.cpp
+++ DayZLifeClient/config.cpp
@@ -148,29 +148,25 @@ class CfgVehicles {
         };
     };
     class DZLStoneOre_ColorBase: Inventory_Base {
-        model="\dz\gear\consumables\Stone.p3d";
-        animClass="NoFireClass";
-        weight=1500;
-        itemSize[]={2,2};
-        canBeSplit=1;
-        varQuantityDestroyOnMin=1;
-        varQuantityInit=1;
-        varQuantityMin=0;
-        varQuantityMax=32;
-        varStackMax=1;
-        isMeleeWeapon=1;
-        hiddenSelections[]=  {
+        model = "\dz\gear\consumables\Stone.p3d";
+        animClass = "NoFireClass";
+        weight = 1500;
+        itemSize[] = {2, 2};
+        canBeSplit = 1;
+        varQuantityDestroyOnMin = 1;
+        varQuantityInit = 1;
+        varQuantityMin = 0;
+        varQuantityMax = 32;
+        varStackMax = 1;
+        isMeleeWeapon = 1;
+        hiddenSelections[] = {
             "zbytek"
         };
-        class DamageSystem
-        {
-            class GlobalHealth
-            {
-                class Health
-                {
-                    hitpoints=200;
-                    healthLevels[]=
-                    {
+        class DamageSystem {
+            class GlobalHealth {
+                class Health {
+                    hitpoints = 200;
+                    healthLevels[] = {
                         {
                             1,
 
@@ -211,23 +207,19 @@ class CfgVehicles {
         };
     };
     class DZLOre_ColorBase: Inventory_Base {
-        model="\dz\gear\consumables\SmallStone.p3d";
-        animClass="NoFireClass";
-        weight=500;
-        itemSize[]={1,1};
-        rotationFlags=17;
-        hiddenSelections[]=  {
+        model = "\dz\gear\consumables\SmallStone.p3d";
+        animClass = "NoFireClass";
+        weight = 500;
+        itemSize[] = {1, 1};
+        rotationFlags = 17;
+        hiddenSelections[] = {
             "zbytek"
         };
-        class DamageSystem
-        {
-            class GlobalHealth
-            {
-                class Health
-                {
-                    hitpoints=100;
-                    healthLevels[]=
-                    {
+        class DamageSystem {
+            class GlobalHealth {
+                class Health {
+                    hitpoints = 100;
+                    healthLevels[] = {
 
                         {
                             1,
@@ -273,32 +265,32 @@ class CfgVehicles {
             };
         };
     };
-	class DZL_Stone: DZLStoneOre_ColorBase {
-		scope = 2;
-		displayName = "$STR_CfgVehicles_Stone0";
-		descriptionShort = "$STR_CfgVehicles_Stone1";
-	};
+    class DZL_Stone: DZLStoneOre_ColorBase {
+        scope = 2;
+        displayName = "$STR_CfgVehicles_Stone0";
+        descriptionShort = "$STR_CfgVehicles_Stone1";
+    };
     class DZL_Gold: DZLStoneOre_ColorBase {
-		scope = 2;
-		displayName = "#Gold";
-		descriptionShort = "#Gold_desc";
-		hiddenSelectionsTextures[]= {
-			"DayZLifeClient\model\Ore\Gold\Gold_Stone.paa"
-		};
+        scope = 2;
+        displayName = "#Gold";
+        descriptionShort = "#Gold_desc";
+        hiddenSelectionsTextures[] = {
+            "DayZLifeClient\model\Ore\Gold\Gold_Stone.paa"
+        };
     };
     class DZL_Gold: DZLOre_ColorBase {
-		scope = 2;
-		displayName = "#Gold";
-		descriptionShort = "#Gold_desc";
-		hiddenSelectionsTextures[]= {
-			"DayZLifeClient\model\Ore\Gold\Gold_Stone_Small.paa"
-		};
+        scope = 2;
+        displayName = "#Gold";
+        descriptionShort = "#Gold_desc";
+        hiddenSelectionsTextures[] = {
+            "DayZLifeClient\model\Ore\Gold\Gold_Stone_Small.paa"
+        };
     };
     class DZL_Copper: DZLStoneOre_ColorBase {
         scope = 2;
         displayName = "#Copper_ore";
         descriptionShort = "#Copper_ore_desc";
-        hiddenSelectionsTextures[]= {
+        hiddenSelectionsTextures[] = {
             "DayZLifeClient\model\Ore\Copper\Copper_Stone.paa"
         };
     };
@@ -306,7 +298,7 @@ class CfgVehicles {
         scope = 2;
         displayName = "#Copper";
         descriptionShort = "#Copper_desc";
-        hiddenSelectionsTextures[]= {
+        hiddenSelectionsTextures[] = {
             "DayZLifeClient\model\Ore\Copper\Copper.paa"
         };
     };
@@ -314,23 +306,23 @@ class CfgVehicles {
         scope = 2;
         displayName = "#Coal";
         descriptionShort = "#Coal_desc";
-        hiddenSelectionsTextures[]= {
+        hiddenSelectionsTextures[] = {
             "DayZLifeClient\model\Ore\Coal\coal_stone.paa"
         };
     };
-	class DZL_Ore_Iron: DZLStoneOre_ColorBase {
-		scope = 2;
-		displayName = "#Iron_ore";
-		descriptionShort = "#Iron_ore_desc";
-        hiddenSelectionsTextures[]= {
+    class DZL_Ore_Iron: DZLStoneOre_ColorBase {
+        scope = 2;
+        displayName = "#Iron_ore";
+        descriptionShort = "#Iron_ore_desc";
+        hiddenSelectionsTextures[] = {
             "DayZLifeClient\model\Ore\Iron\iron_stone.paa"
         };
-	};
+    };
     class DZL_Iron: DZLOre_ColorBase {
         scope = 2;
         displayName = "#Iron";
         descriptionShort = "#Iron_desc";
-        hiddenSelectionsTextures[]= {
+        hiddenSelectionsTextures[] = {
             "DayZLifeClient\model\Ore\Iron\Iron.paa"
         };
     };
@@ -338,7 +330,7 @@ class CfgVehicles {
         scope = 2;
         displayName = "#raw_Diamond";
         descriptionShort = "#raw_Diamond_desc";
-        hiddenSelectionsTextures[]= {
+        hiddenSelectionsTextures[] = {
             "DayZLifeClient\model\Ore\Diamond\Diamond_Stone.paa"
         };
     };
@@ -346,32 +338,28 @@ class CfgVehicles {
         scope = 2;
         displayName = "#Diamond_cut";
         descriptionShort = "#Diamond_cut_desc";
-        hiddenSelectionsTextures[]= {
+        hiddenSelectionsTextures[] = {
             "DayZLifeClient\model\Ore\Diamond\Diamond_Stone.paa"
         };
     };
-	class DZL_Cement: Inventory_Base {
-		scope = 2;
-		displayName = "#Cement";
-		descriptionShort = "#Cement";
-		model = "\DayZLifeClient\model\Cement\DZL_Cement.p3d";
-		weight = 20000;
-		itemSize[] = {2,2};
-		hiddenSelections[]=  {
+    class DZL_Cement: Inventory_Base {
+        scope = 2;
+        displayName = "#Cement";
+        descriptionShort = "#Cement";
+        model = "\DayZLifeClient\model\Cement\DZL_Cement.p3d";
+        weight = 20000;
+        itemSize[] = {2, 2};
+        hiddenSelections[] = {
             "camoDZL"
         };
-        hiddenSelectionsTextures[]= {
+        hiddenSelectionsTextures[] = {
             "DayZLifeClient\model\Cement\Data\DZL_Cement.paa"
         };
-        class DamageSystem
-        {
-            class GlobalHealth
-            {
-                class Health
-                {
-                    hitpoints=200;
-                    healthLevels[]=
-                    {
+        class DamageSystem {
+            class GlobalHealth {
+                class Health {
+                    hitpoints = 200;
+                    healthLevels[] = {
                         {
                             1,
 
@@ -408,5 +396,5 @@ class CfgVehicles {
                 };
             };
         };
-	};
+    };
 };
