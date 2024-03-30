e2fef5fc38e7214ab16e65a95a160dce5f5400ee
TheBuster <RalfGe@Ihr-it-projekt.de>
2024-03-30 15:15:19 +0100

    fix null pointer

--- DayZLifeServer/scripts/4_World/Database/DZLTraderStorage.c
+++ DayZLifeServer/scripts/4_World/Database/DZLTraderStorage.c
@@ -1,7 +1,9 @@
 modded class DZLTraderStorage {
     void Insert(DZLTraderType item) {
         DZLTraderTypeStorage type = new DZLTraderTypeStorage(item);
+        if (!storageArray) storageArray = new array<ref DZLTraderTypeStorage>;
         storageArray.Insert(type);
+        if (!storageMap) storageMap = new map<string, ref DZLTraderTypeStorage>;
         storageMap.Set(type.GetType(), type);
     }
 }
