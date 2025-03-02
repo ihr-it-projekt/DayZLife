dbbc474b295f21b8b0b168ecacb0793ed0f9aee4
TheBuster <RalfGe@Ihr-it-projekt.de>
2025-03-02 19:21:06 +0100

    update for new revive version

--- DayZLifeServer/scripts/4_World/EventListener/DZLMedicHelpListener.c
+++ DayZLifeServer/scripts/4_World/EventListener/DZLMedicHelpListener.c
@@ -3,12 +3,6 @@ class DZLMedicHelpListener: DZLBaseEventListener {
 #ifdef TBRevivePlayerServer
     override void OnRPC(PlayerIdentity sender, Object target, int rpc_type, ParamsReadContext ctx) {
         if(rpc_type == DZL_RPC.MEDIC_CALL) {
-            PlayerBase emergencyPlayer = PlayerBase.Cast(target);
-            if(!emergencyPlayer) return;
-
-            TBREmergency emergency = new TBREmergency(emergencyPlayer);
-            emergency.isMedic = true;
-            TBREmergencies.Get().Add(emergency);
             DZLSendMessage(sender, "#medics_was_called.");
             DZLSendMedicMessage("#there_is_a_new_emergency");
             return;
@@ -16,19 +10,13 @@ class DZLMedicHelpListener: DZLBaseEventListener {
         if(rpc_type == DZL_RPC.GET_EMERGENCY_CALLS) {
             array<Man> players = new array<Man>;
             g_Game.GetPlayers(players);
-
-            TBREmergencies emergencies = TBREmergencies.Get();
-            array<ref DZLOnlinePlayer> onlineEmergencies = new array<ref DZLOnlinePlayer>;
+			array<ref DZLOnlinePlayer> onlineEmergencies = new array<ref DZLOnlinePlayer>;
+            
             foreach(Man _player: players) {
-                string ident = _player.GetIdentity().GetId();
+                PlayerBase player = PlayerBase.Cast(_player);
+				if (!player || !player.tbrIsDead) continue;
 
-                if(!emergencies.GetEmergency(ident)) continue;
-                if(_player.GetHealth() > 1) {
-                    emergencies.Remove(ident);
-                    continue;
-                }
-
-                onlineEmergencies.Insert(new DZLOnlinePlayer(ident, _player.GetIdentity().GetName(), "", _player.GetPosition()));
+                onlineEmergencies.Insert(new DZLOnlinePlayer(player.TBLibGetPlayerId(), player.TBLibGetPlayerName(), "", player.GetPosition()));
             }
             g_Game.RPCSingleParam(null, DZL_RPC.GET_EMERGENCY_CALLS_RESPONSE, new Param1<ref array<ref DZLOnlinePlayer>>(onlineEmergencies), true, sender);
         }
